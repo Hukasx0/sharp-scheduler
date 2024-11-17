e892cba075a528be8ca4d6a65242be5c10f620ec
Hubert Kasperek <hubertkasp13@gmail.com>
2024-11-17 13:52:47 +0100

    Add basic CRUD operations on scheduled jobs to the API

---
+++ sharp-scheduler.Server/Controllers/ScheduledJobsController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using sharp_scheduler.Server.Data;
+using sharp_scheduler.Server.DTOs;
+using sharp_scheduler.Server.Models;
+
+namespace sharp_scheduler.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScheduledJobsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ScheduledJobsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var jobs = await _context.ScheduledJobs.ToListAsync();
+            return Ok(jobs);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ScheduledJobPostDTO newJob)
+        {
+            var job = new ScheduledJob
+            {
+                Name = newJob.Name,
+                Command = newJob.Command,
+                CronExpression = newJob.CronExpression,
+                CreatedAt = DateTime.UtcNow,
+                LastResult = ""
+            };
+
+            using (var transaction = await  _context.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    await _context.ScheduledJobs.AddAsync(job);
+                    await _context.SaveChangesAsync();
+
+                    await transaction.CommitAsync();
+
+                    return CreatedAtAction(nameof(GetAll), new { id = job.Id }, job);
+                }
+                catch (Exception ex)
+                {
+                    await transaction.RollbackAsync();
+                    Console.WriteLine(ex.ToString());
+                    throw;
+                }
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, ScheduledJobPostDTO updateJob)
+        {
+            var existingJob = await _context.ScheduledJobs.FindAsync(id);
+            if (existingJob == null) return NotFound();
+
+            existingJob.Name = updateJob.Name;
+            existingJob.Command = updateJob.Command;
+            existingJob.CronExpression = updateJob.CronExpression;
+
+            _context.Entry(existingJob).State = EntityState.Modified;
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var job = await _context.ScheduledJobs.FindAsync(id);
+            if (job == null) return NotFound();
+
+            _context.ScheduledJobs.Remove(job);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
--- sharp-scheduler.Server/DTOs/ScheduledJobDTO.cs
+++ sharp-scheduler.Server/DTOs/ScheduledJobDTO.cs
@@ -1,6 +1,6 @@
-﻿namespace sharp_scheduler.Server.Models
+﻿namespace sharp_scheduler.Server.DTOs
 {
-    public class ScheduledJob
+    public class ScheduledJobDTO
     {
         public int Id { get; set; }
         public string Name { get; set; } = string.Empty;
@@ -9,5 +9,12 @@
         public DateTime CreatedAt { get; set; }
         public DateTime? LastExecution { get; set; }
         public string? LastResult { get; set; }
+    }
+
+    public class ScheduledJobPostDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Command { get; set; } = string.Empty;
+        public string CronExpression { get; set; } = string.Empty;
     }
 }
